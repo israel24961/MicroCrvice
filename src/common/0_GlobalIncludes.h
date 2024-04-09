4882262c450a2d775906c8fc4ff032928eca7dbb
IAT <israel24961@hotmail.com>
2024-04-10 00:06:15 +0200

    Init

---
+++ src/common/0_GlobalIncludes.h
@@ -0,0 +1,22 @@
+#pragma once
+// clang-format off
+// clang-format on
+
+#include "log.h"
+#include <GLFW/glfw3.h>
+#include <assert.h>
+#include <ctype.h>
+#include <easycringelib.h>
+#include <float.h>
+#include <libgen.h>
+#include <log.h>
+#include <math.h>
+#include <raylib.h>
+#include <raymath.h>
+#include <rlgl.h>
+#include <search.h>
+#include <signal.h>
+#include <string.h>
+#include <threads.h>
+#include <time.h>
+#include <unistd.h>
